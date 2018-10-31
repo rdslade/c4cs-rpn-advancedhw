bff959402254deb5cad93ad04c5bde26322237d4
rdslade <rdslade@umich.edu>
2018-10-31 19:01:10 -0400

    Fixed test file
    Errors with tabs/spaces in test_rpn now fixed

--- test_rpn.py
+++ test_rpn.py
@@ -15,8 +15,9 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
-    def test_exp(self):
-	result = rpn.calculate("2 4 ^")
-	self.assertEqual(16, result)
-	result = rpn.calculate("65 0 ^")
-	self.assertEqual(1, result) 
+    def test_exponent(self):
+        result = rpn.calculate("2 4 ^")
+        self.assertEqual(16, result)
+
+if __name__ == '__main__':
+    unittest.main()
